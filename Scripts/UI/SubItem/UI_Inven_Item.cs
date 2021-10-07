49cc04cdaca05fa20e760bdec5a76f2d8e7f9c4b
talenale <80935423+talenale@users.noreply.github.com>
2021-10-08 07:43:16 +0900

    Add files via upload

---
+++ Scripts/UI/SubItem/UI_Inven_Item.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Inven_Item : UI_Base
+{
+    enum GameObjects
+    {
+        ItemIcon,
+        ItemNameText,
+    }
+
+    string _name;
+
+    public override void Init()
+    {
+        Bind<GameObject>(typeof(GameObjects));
+        Get<GameObject>((int)GameObjects.ItemNameText).GetComponent<Text>().text = _name;
+
+        Get<GameObject>((int)GameObjects.ItemIcon).BindEvent((PointerEventData) => { Debug.Log($"아이템 클릭! {_name}"); });
+    }
+
+    public void SetInfo(string name)
+    {
+        _name = name;
+    }
+}
---
+++ Scripts/Utils/Define.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum WorldObject
+    {
+        Unknown,
+        Player,
+        Monster,
+    }
+
+	public enum State
+	{
+		Die,
+		Moving,
+		Idle,
+		Skill,
+	}
+
+    public enum Layer
+    {
+        Monster = 8,
+        Ground = 9,
+        Block = 10,
+    }
+
+    public enum Scene
+    {
+        Unknown,
+        Login,
+        Lobby,
+        Game,
+    }
+
+    public enum Sound
+    {
+        Bgm,
+        Effect,
+        MaxCount,
+    }
+
+    public enum UIEvent
+    {
+        Click,
+        Drag,
+    }
+
+    public enum MouseEvent
+    {
+        Press,
+        PointerDown,
+        PointerUp,
+        Click,
+    }
+
+    public enum CameraMode
+    {
+        QuarterView,
+    }
+}
